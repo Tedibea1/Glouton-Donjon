bbfe8191927a05f0aa7c85b84d83251601b42e11
Tedibea <67254761+Tedibea1@users.noreply.github.com>
2026-04-18 15:56:38 +0200

    script.js

--- script.js
+++ script.js
@@ -775,7 +775,7 @@ function renderGame() {
     ['CAP', effectiveStats.CAP],
     ['META', effectiveStats.META],
     ['ATK', derived.atk],
-    ['DEF', derived.def],
+    ['RES', derived.def],
     ['CRIT', `${derived.crit}%`],
     ['ESQ', `${derived.evasion}%`]
   ].map(([k,v]) => `<div class=\"combat-stat-line\"><strong>${k} :</strong><span>${v}</span></div>`).join('');
